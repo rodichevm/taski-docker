2b559083f89072b69af88a716fc8b7d42985c916
Michael Rodichev <rodichevmichael@gmail.com>
2023-09-10 16:38:02 +0300

    fixed tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -2,18 +2,20 @@ from http import HTTPStatus
 
 from django.test import Client, TestCase
 
-from . import models
+from backend.api import models
 
 
 class TaskiAPITestCase(TestCase):
-    def setUP(self):
+    def setUp(self):
         self.guest_client = Client()
 
     def test_list_exists(self):
+        """Проверка доступности списка задач."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
+        """Проверка создания задачи."""
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
